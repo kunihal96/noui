de10f6ba68fb272d6c9008b987ca5a14026dc340
badal singh <prithvis811@gmail.com>
2020-05-31 20:36:46 +0530

    usercase one

--- empl.java
+++ empl.java
@@ -2,6 +2,12 @@ public class empl
 {
 public static void main(String []args)
 {
+int fulltime=1;
 System.out.println("welcome to employee wage computation");
+double emp=Math.floor(Math.random() * 10 ) % 2;
+if (emp==fulltime)
+	System.out.println("employee is present");
+else
+        System.out.println("employee is absent");
 }
 }
